d258ae3a10dcf9684359a13fe0d9bd882fc87945
Marcus Zanona <zanona@me.com>
2016-09-10 19:04:36 +0100

    Improve CLI

--- cli.js
+++ cli.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 var map = require('./'),
     args = process.argv.splice(2),
-    path = require('path'),
-    doc = require(path.resolve(args[0]));
+    schema = require(require('path').resolve(args[0])),
+    path = args[1];
 
-if (args[1]) { doc = doc[args[1]]; }
-console.log(JSON.stringify(map(doc), null, 2));
+//if (path) path.split(/[\[\]\.]/).filter((p) => p).forEach((p) => doc = doc[p]);
+if (schema.definitions) {
+  console.log(JSON.stringify(map(schema.definitions, path), null, 2));
+} else {
+  console.error('definitions not found in schema');
+}
 
