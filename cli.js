b05311ba93284248b1730fa92fe51791be5b0c0f
Marcus Zanona <zanona@me.com>
2016-09-18 18:44:03 +0100

    Add support for stdin piping (jsderef schema.json | swagger-es-mapper)

--- cli.js
+++ cli.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
-var map = require('./'),
-    args = process.argv.splice(2),
-    schema = require(require('path').resolve(args[0])),
-    path = args[1];
+'use strict';
+const path = require('path'),
+      map = require('./'),
+      args = process.argv.splice(2),
+      throughPipe = !process.stdin.isTTY;
 
-//if (path) path.split(/[\[\]\.]/).filter((p) => p).forEach((p) => doc = doc[p]);
-if (schema.definitions) {
-  console.log(JSON.stringify(map(schema.definitions, path), null, 2));
+function run(schema, model) {
+  if (schema.definitions) {
+    console.log(JSON.stringify(map(schema.definitions, model), null, 2));
+  } else {
+    console.error('definitions not found in schema');
+  }
+}
+
+if (throughPipe) {
+  let input = '';
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', function () {
+    const chunk = process.stdin.read();
+    if (chunk !== null) { input += chunk; }
+  });
+  process.stdin.on('end', () => run(JSON.parse(input), args[0]));
 } else {
-  console.error('definitions not found in schema');
+  run(require(path.resolve(args[0]), args[1]));
 }
 
